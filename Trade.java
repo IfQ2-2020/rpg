d90a5b8ec42fd319f1a3b91cd37b9dddce8d7d6a
moe <julius.koechling@kghalle.de>
2020-09-08 13:32:05 +0100

    trade base

---
+++ Trade.java
@@ -0,0 +1,25 @@
+public class Trade
+{
+    public int ware[];
+
+    /**
+     * Konstruktor für Objekte der Klasse Trade
+     */
+    public Trade()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
