2499fcf715cb5391c9bacdf66ecc3e290026a9b0
Mattes <Mattes.Lahmann@outlook.com>
2020-09-08 13:15:37 +0100

    Items kommen

---
+++ Combats.java
@@ -0,0 +1,15 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Combats.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Combats extends Items
+{
+    private int durability;
+    private int damage;
+    private int range;
+    
+    public Combats(
+}
---
+++ Food.java
@@ -0,0 +1,16 @@
+
+/**
+ * Beschreiben Sie hier die Klasse food.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Food extends Items
+{
+    int Naehrwert;
+    
+    public Food(int p_Naerhwert)
+    {
+        Naehrwert = p_Naehrwert
+    }
+}
