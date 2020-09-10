566466e7747e827b99e8f9db192f51804b9974a0
Mattes <Mattes.Lahmann@outlook.com>
2020-09-10 07:58:45 +0100

    k

--- NPC.java
+++ NPC.java
@@ -1,16 +1,27 @@
 public class NPC
 {
-  private int ID;
-  private int[] position = new int[3];
-  private String name;
+    private int ID;
+    private int[] position = new int[3];
+    private String name;
   
-  public NPC(int _ID, int[] _position, String _name){
-         ID = _ID;
-         position = _position;
-         name = _name;
-  }
+    public NPC(int _ID, int[] _position, String _name){
+        ID = _ID;
+        position = _position;
+        name = _name;
+    }
+
+    public int[] getPosition(){
+        return position;
+    }
+    public String getName(){
+        return name;
+    }
+    
+    public int getID(){
+        return ID;
+    }
   
-  /*public int getTexture(){
-         return Texture.getID();
-  }*/
+    /*public int getTexture(){
+    /*    return Texture.getID();
+    }*/
 }
