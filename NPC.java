33a275691d4481e418b387e328b8420bab679b52
JK <julius.koechling@kghalle.de>
2020-09-10 07:46:51 +0100

    tqabs

--- NPC.java
+++ NPC.java
@@ -1,29 +1,27 @@
 public class NPC
 {
-  private int ID;
-  private int[] position = new int[3];
-  private String name;
+    private int ID;
+    private int[] position = new int[3];
+    private String name;
   
-  public NPC(int _ID, int[] _position, String _name){
-         ID = _ID;
-         position = _position;
-         name = _name;
-  }
-<<<<<<< HEAD
-  public void getTexture(){
-         //return Texture.ID
-  }
-  public int[] getPosition(){
-    return position;
+    public NPC(int _ID, int[] _position, String _name){
+        ID = _ID;
+        position = _position;
+        name = _name;
     }
-  public String getName(){
-    return name;}
-  public int getID(){
-    return ID;}
-=======
+
+    public int[] getPosition(){
+        return position;
+    }
+    public String getName(){
+        return name;
+    }
+    
+    public int getID(){
+        return ID;
+    }
   
-  /*public int getTexture(){
-         return Texture.getID();
-  }*/
->>>>>>> 3a0be07cf18c85b9c250e1e73187b0c795967978
+    /*public int getTexture(){
+    /*    return Texture.getID();
+    }*/
 }
