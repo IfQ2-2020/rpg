7eea9b2d7b90b632ccef3b22ff88e5d6470c2058
JK <julius.koechling@kghalle.de>
2020-09-10 07:55:26 +0100

    trade tabs TM

--- Trade.java
+++ Trade.java
@@ -1,11 +1,12 @@
 public class Trade
 {
-  private int[] ware = new int[2];
-  public Trade(int ware1, int ware2){
-     ware[0] = ware1;
-     ware[1] = ware2;
-  }
-  public int[] getWare(){
-         return ware;
-  }
+    private int[] ware = new int[2];
+    public Trade(int ware1, int ware2){
+        ware[0] = ware1;
+        ware[1] = ware2;
+    }
+    
+    public int[] getWare(){
+        return ware;
+    }
 }
