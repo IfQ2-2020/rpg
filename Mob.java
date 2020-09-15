294e0d6c7403509cd88dd3865d4dac9c9c79be73
TB <tom.benninghoff@gmail.com>
2020-09-15 13:23:09 +0100

    npc

---
+++ Mob.java
@@ -0,0 +1,31 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Mob.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Mob extends NPC
+{
+    private int agressionlv;
+    private int health;
+    private int strength;
+    private int lootingmass;
+    private int[] lootingtable = new int[lootingmass];
+    
+    
+
+    /**
+     * Konstruktor für Objekte der Klasse Mob
+     */
+    public Mob(int pID,int pSpeed , int[] position, String name, int pAgressionlv, int pHealth, int pStrength, int pLootingmass)
+    {
+        super(pSpeed, pID, position[0], position[1], position[2], name);
+        agressionlv = pAgressionlv;
+        health = pHealth;
+        strength = pStrength;
+        lootingmass = pLootingmass;
+    }
+
+    
+}
--- NPC.java
+++ NPC.java
@@ -3,15 +3,17 @@ public class NPC
   private int ID;
   private int[] position = new int[3];
   private String name;
+  private int speed;
   //private Texture mTexture;
 
 
-  public NPC(int _ID, int position1, int position2, int direction, String _name){
+  public NPC(int _ID,int pSpeed, int position1, int position2, int direction, String _name){
          ID = _ID;
          position[0] = position1;
          position[1] = position2;
          position[2] = direction;
          name = _name;
+         speed = pSpeed;
   }
   public void setPosition(int[] sPosition){
     position = sPosition;
