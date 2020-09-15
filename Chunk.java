d1b53b1c6cfe58d13229b04a2cf09eef1e4e631d
JS <janniksalzwedel@gmail.com>
2020-09-15 13:30:07 +0100

    a

--- Chunk.java
+++ Chunk.java
@@ -2,28 +2,53 @@ public class Chunk{
 
     private Tile[] tiles;
     private int[] position;// von tile oben links
+    private boolean isFirstChunk;
     
-    public Chunk(Chunk[] pNextChunks, int[] pPosition){
+    public Chunk(Chunk[] pNextChunks, int[] pPosition, boolean pisFirstChunk){
         //NextCHunks: oben 0 links 1 rechts 2 unten 3
-        tiles = createTiles(pNextChunks);
+        isFirstChunk = pisFirstChunk;
+        tiles = createTiles(pNextChunks, isFirstChunk);
         position = pPosition;
+        
     }
     
-    private Tile[] createTiles(Chunk[] pNextChunks){
+    private Tile[] createTiles(Chunk[] pNextChunks, boolean isFirst){
         Tile[] ret = new Tile[16*16];
         Chunk[] nextChunks = pNextChunks;
-        
-        for(int y = 0; y < 16; y++){
-            for(int x = 0; x < 16; x++){
-                
-                ret[returnTilenumber(x,y)] = new Tile(0);
-                
-                if(x == 0){
-                    ret[returnTilenumber(x,y)] = new Tile(1); 
+        if(!isFirst){
+            for(int y = 0; y < 16; y++){
+                for(int x = 0; x < 16; x++){
+                    
+                    ret[returnTilenumber(x,y)] = new Tile(0);
+                    if(nextChunks[0] != null && y == 0){
+                       ret[returnTilenumber(x,y)] =  nextChunks[0].tiles[
+                                                    returnTilenumber(x,15)];
+                    }
+                    if(nextChunks[1] != null && x == 0){
+                        ret[returnTilenumber(x,y)] =  nextChunks[1].tiles[
+                                                    returnTilenumber(0,y)];
+                    }
+                    if(nextChunks[2] != null && x == 15){
+                        ret[returnTilenumber(x,y)] =  nextChunks[2].tiles[
+                                                    returnTilenumber(15,y)];
+                    }
+                    if(nextChunks[3] != null && y == 15){
+                        ret[returnTilenumber(x,y)] =  nextChunks[3].tiles[
+                                                    returnTilenumber(x,0)];
+                    }
+                }
+            }
+        }else{
+            for(int y = 0; y < 16; y++){
+                for(int x = 0; x < 16; x++){
+                    ret[returnTilenumber(x,y)] = new Tile(0);
+                    
+                    if(x == 0 || x == 15 || y == 0 || y == 15){
+                        ret[returnTilenumber(x,y)] = new Tile(1); 
+                    }
                 }
             }
         }
-        
         return ret;
     }
     
--- World.java
+++ World.java
@@ -14,14 +14,14 @@ public class World
     }
     
     private void loadFirstChunks(){
-        loadedChunks[0] = new Chunk(null,new int[]{0,0});
-        loadedChunks[1] = new Chunk(new Chunk[]{loadedChunks[0],null,null,null},
-                                    new int[]{0,16});
-        loadedChunks[2] = new Chunk(new Chunk[]{null,loadedChunks[0],null,null},
-                                    new int[]{-16,0});
-        loadedChunks[3] = new Chunk(new Chunk[]{null,null,loadedChunks[0],null},
-                                    new int[]{16,0}); 
-        loadedChunks[4] = new Chunk(new Chunk[]{null,null,null,loadedChunks[0]},
-                                    new int[]{0,-16});                            
+        loadedChunks[0] = new Chunk(null,new int[]{0,0}, true);
+        loadedChunks[1] = new Chunk(new Chunk[]{null,null,null,loadedChunks[0]},
+                                    new int[]{0,16}, false);
+        loadedChunks[2] = new Chunk(new Chunk[]{null,null,loadedChunks[0],null},
+                                    new int[]{-16,0}, false);
+        loadedChunks[3] = new Chunk(new Chunk[]{null,loadedChunks[0],null,null},
+                                    new int[]{16,0}, false); 
+        loadedChunks[4] = new Chunk(new Chunk[]{loadedChunks[0],null,null,null},
+                                    new int[]{0,-16}, false);                            
     }
 }
